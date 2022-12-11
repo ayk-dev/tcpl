698a48c285711f6d594714da05e42c672b746c96
Albena <albena.y.krasteva@gmail.com>
2022-12-11 20:57:30 +0200

    Added symbolic constants

--- exercise1-5.c
+++ exercise1-5.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 int main() {
     /* Print Fahrenheit - Celsius table from 0... 300 
     formula C = (5/9)*(F - 32) */
@@ -7,7 +11,7 @@ int main() {
     float cels;
 
     printf("Fahrenheit to Celsius conversion table:\n");
-    for (float fahr = 0.0; fahr <= 300.0; fahr += 20) {
+    for (float fahr = LOWER; fahr <= UPPER; fahr += STEP) {
         cels = (5.0 / 9.0) * (fahr - 32);
         printf("\t%3.0f\t%6.1f\n", fahr, cels);
     }
